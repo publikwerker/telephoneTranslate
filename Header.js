0762b8b9ccdfb8d7e7e1cad8575820c129b996bc
Publikwerker <jason.michael.hoffman@gmail.com>
2023-01-16 08:46:27 -0800

    installed React-Native and Expo

---
+++ Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = () => {
+    return (
+        <View>
+            <Text> Telephone Translate</Text>
+            <Text>The Familiar Game with a Modern Twist</Text>
+        </View>
+    );  
+};
+
+export default Header;
